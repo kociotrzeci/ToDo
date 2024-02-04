a37d53a2030a5279c7b2210e4c32aecea63ab5df
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-02-04 21:32:01 +0100

    quick fix

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import { Notebook } from './logic.js'
 const notebook = new Notebook;
 notebook.addProject('projekt1')
 notebook.addNote('title', 'desc', '2000-20-20', 1, 0);
-notebook.addNote('title', 'desc', '2000-20-20', 1, 0); notebook.addNote('title', 'desc', '2000-20-20', 1, 0); notebook.addNote('title', 'desc', '2000-20-20', 1, 0); notebook.addNote('title', 'desc', '2000-20-20', 1, 0);
+notebook.addNote('title', 'desc', '2000-20-20', 1, 0);
+notebook.addNote('title', 'desc', '2000-20-20', 1, 0);
+notebook.addNote('title', 'desc', '2000-20-20', 1, 0);
+notebook.addNote('title', 'desc', '2000-20-20', 1, 0);
 notebook.printNotes();
