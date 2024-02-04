78b6633d494dace12a9cdbeb7ba70a327c04980a
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-02-04 21:21:50 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles.scss";
+import { Notebook } from './logic.js'
+
+const notebook = new Notebook;
+notebook.addProject('projekt1')
+notebook.addNote('2000-12-12', 0, 'notatka');
+notebook.addNote('2000-12-12', 0, 'notatka');
+notebook.addNote('2000-12-12', 0, 'notatka');
+notebook.printNotes();
---
+++ src/logic.js
@@ -0,0 +1,53 @@
+export class Notebook {
+    constructor() {
+        this.notes = [];
+        this.projects = [];
+    }
+    addNote(title, description, dueDate, priority, projectID) {
+        this.projects[projectID].addNote(Note.id);
+        this.notes.push(new Note(date, textContent));
+
+    }
+    addProject(projectName) {
+        this.projects.push(new Project(projectName));
+    }
+    printNotes() {
+        this.notes.forEach(element => {
+            console.log(element);
+        });
+    }
+}
+
+
+
+class Note {
+    static id = 0;
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.id = Note.id;
+        Note.id++;
+        this.isComplete = false;
+    }
+    print(params) {
+        console.log(this);
+    }
+}
+
+class Project {
+    static id = 0;
+
+    constructor(projectName) {
+        this.projectName = projectName;
+        this.notes = [];
+
+        this.id = Project.id;
+        Project.id++;
+    }
+    addNote(noteID) {
+        this.notes.push(noteID);
+    }
+}
+
