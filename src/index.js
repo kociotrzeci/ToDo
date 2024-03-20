0b25630c8ad221c198ba2287d521a96fbe9196d8
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-03-20 21:04:18 +0100

    dui

--- src/index.js
+++ src/index.js
@@ -1,1 +1,74 @@
 import "./styles.scss";
+const intervalTime = 5000;
+const button = document.querySelector("button");
+const menu = document.querySelector(".menu-container");
+button.addEventListener("click", () => {
+  if (menu.classList.contains("visible")) {
+    menu.classList.remove("visible");
+  } else {
+    menu.classList.add("visible");
+  }
+});
+const carousel = document.querySelector(".carousel");
+carousel.position = 0;
+carousel.numberOfChilds = carousel.children.length;
+carousel.move = function (direction) {
+  if (direction === "left") {
+    carousel.position--;
+    if (carousel.position < 0) {
+      carousel.position = carousel.numberOfChilds - 1;
+    }
+  } else if (direction === "right") {
+    carousel.position++;
+    if (carousel.position === carousel.numberOfChilds) {
+      carousel.position = 0;
+    }
+  } else {
+    carousel.position = direction;
+  }
+  carousel.className = "carousel";
+  carousel.classList.add(carousel.position);
+  console.log(carousel.position);
+  carousel.style.left = `-${carousel.position * 512}px`;
+  navbar.fillCircle();
+  clearInterval(carousel.interval);
+  carousel.interval = setInterval(function () {
+    carousel.move("right");
+  }, intervalTime);
+};
+
+const navbar = document.querySelector(".navbar");
+navbar.populate = function () {
+  for (let i = 0; i < carousel.numberOfChilds; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+
+    dot.addEventListener("click", () => {
+      carousel.move(i);
+      navbar.fillCircle();
+    });
+    navbar.appendChild(dot);
+  }
+};
+navbar.fillCircle = function () {
+  console.log(navbar.querySelectorAll(".dot"));
+  for (let i = 0; i < carousel.numberOfChilds; i++) {
+    navbar.querySelectorAll(".dot")[i].classList.remove("filled");
+  }
+  navbar.querySelectorAll(".dot")[carousel.position].classList.add("filled");
+};
+navbar.populate();
+navbar.fillCircle();
+
+const leftButton = document.querySelector("button#left");
+leftButton.addEventListener("click", () => {
+  carousel.move("left");
+});
+const rightButton = document.querySelector("button#right");
+rightButton.addEventListener("click", () => {
+  carousel.move("right");
+});
+
+carousel.interval = setInterval(function () {
+  carousel.move("right");
+}, intervalTime);
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/images", to: "images" }, // Replace with your image folder paths
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.scss$/i,
-                use: ['style-loader', 'css-loader', "sass-loader",],
-            },
-        ]
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    }
-
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
 };
